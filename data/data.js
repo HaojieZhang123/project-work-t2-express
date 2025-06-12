9d8f457f8f265bc8edbf80a6801b837f4244bd7d
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-12 10:23:59 +0200

    [UPD] creata la connessione al db

---
+++ data/data.js
@@ -0,0 +1,12 @@
+//importo mysql2
+const mysql = require('mysql2');
+
+//creo la connessione al db
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
